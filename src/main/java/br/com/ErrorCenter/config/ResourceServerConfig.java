b6d971667601167c92bc56cd447451281803302d
Luis Carvalho <luiscvlh11@gmail.com>
2020-05-04 19:43:31 -0300

    Adicionar swagger basico

--- src/main/java/br/com/ErrorCenter/config/ResourceServerConfig.java
+++ src/main/java/br/com/ErrorCenter/config/ResourceServerConfig.java
@@ -10,11 +10,20 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+    private static final String[] AUTH_WHITELIST = {
+
+            // -- swagger ui
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/v2/api-docs",
+            "/webjars/**"
+    };
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/application").permitAll()
-                .antMatchers("/h2/**").permitAll()
+                .antMatchers(AUTH_WHITELIST).permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin()
                 .and().headers().frameOptions().sameOrigin();
