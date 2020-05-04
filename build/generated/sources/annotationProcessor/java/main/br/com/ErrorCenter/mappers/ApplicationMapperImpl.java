332e2390271bcf26d1f6bacc347b7e08adb37e00
Luis Carvalho <luiscvlh11@gmail.com>
2020-05-04 03:29:48 -0300

    Deploy again

---
+++ build/generated/sources/annotationProcessor/java/main/br/com/ErrorCenter/mappers/ApplicationMapperImpl.java
@@ -0,0 +1,46 @@
+package br.com.ErrorCenter.mappers;
+
+import br.com.ErrorCenter.dtos.ApplicationDTO;
+import br.com.ErrorCenter.entities.ApplicationEntity;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-05-04T03:23:43-0300",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 1.8.0_252 (Private Build)"
+)
+@Component
+public class ApplicationMapperImpl implements ApplicationMapper {
+
+    @Override
+    public ApplicationDTO map(ApplicationEntity applicationEntity) {
+        if ( applicationEntity == null ) {
+            return null;
+        }
+
+        ApplicationDTO applicationDTO = new ApplicationDTO();
+
+        applicationDTO.setCreated_at( applicationEntity.getCreatedAt() );
+        applicationDTO.setId( applicationEntity.getId() );
+        applicationDTO.setName( applicationEntity.getName() );
+
+        return applicationDTO;
+    }
+
+    @Override
+    public List<ApplicationDTO> map(List<ApplicationEntity> applicationEntities) {
+        if ( applicationEntities == null ) {
+            return null;
+        }
+
+        List<ApplicationDTO> list = new ArrayList<ApplicationDTO>( applicationEntities.size() );
+        for ( ApplicationEntity applicationEntity : applicationEntities ) {
+            list.add( map( applicationEntity ) );
+        }
+
+        return list;
+    }
+}
---
+++ build/generated/sources/annotationProcessor/java/main/br/com/ErrorCenter/mappers/EventDetailMapperImpl.java
@@ -0,0 +1,41 @@
+package br.com.ErrorCenter.mappers;
+
+import br.com.ErrorCenter.dtos.ApplicationDTO;
+import br.com.ErrorCenter.dtos.EventDetailDTO;
+import br.com.ErrorCenter.entities.EventEntity;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-05-04T03:23:43-0300",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 1.8.0_252 (Private Build)"
+)
+@Component
+public class EventDetailMapperImpl implements EventDetailMapper {
+
+    @Override
+    public EventDetailDTO map(EventEntity eventEntity, ApplicationDTO applicationDTO) {
+        if ( eventEntity == null && applicationDTO == null ) {
+            return null;
+        }
+
+        EventDetailDTO eventDetailDTO = new EventDetailDTO();
+
+        if ( eventEntity != null ) {
+            eventDetailDTO.setCreated_at( eventEntity.getCreatedAt() );
+            eventDetailDTO.setId( eventEntity.getId() );
+            eventDetailDTO.setDescription( eventEntity.getDescription() );
+            eventDetailDTO.setLevel( eventEntity.getLevel() );
+            eventDetailDTO.setLog( eventEntity.getLog() );
+            if ( eventEntity.getQuantity() != null ) {
+                eventDetailDTO.setQuantity( eventEntity.getQuantity() );
+            }
+        }
+        if ( applicationDTO != null ) {
+            eventDetailDTO.setOrigin( applicationDTO );
+        }
+
+        return eventDetailDTO;
+    }
+}
