b5f6505d30f476be41b3250cc10915bc71c250c2
Luis Carvalho <luiscvlh11@gmail.com>
2020-05-04 16:59:48 -0300

    Agora vai

--- src/main/java/br/com/ErrorCenter/ErrorCenterApplication.java
+++ src/main/java/br/com/ErrorCenter/ErrorCenterApplication.java
@@ -1,10 +1,12 @@
 package br.com.ErrorCenter;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @EnableJpaAuditing
 public class ErrorCenterApplication {
 
